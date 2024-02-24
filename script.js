d47cefc63575fc2d7edf9825a26329c1098aecce
Anunay Argha <anunayargha@gmail.com>
2024-02-24 22:36:45 +0600

    variable declared in js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict'
+"use strict";
+
+// * DATA - 
+
+
+
+// * ELEMENT SELECTIONS -
+
+// * GENERAL ELEMENTS
+
+const balanceAmount = document.getElementById("balance");
+const movementContainer = document.getElementById("movements");
+const welcomeLabel = document.getElementById("welcome");
+
+// * BUTTON ELEMENTS
+
+const loginBtn = document.getElementById("login_btn");
+const transferBtn = document.getElementById("transfer_btn");
+const loanBtn = document.getElementById("loan_btn");
+const closeBtn = document.getElementById("close_btn");
+
+// * INPUT ELEMENTS
+
+const tranferToInput = docuemtn.getElementById("transfer_user");
+const transferAmount = document.getElementById("transfer_amount");
+const loanAmount = document.getElementById("loan_amount");
+const confirmUser = documetn.getElementById("confirm_user");
+const confirmPin = documetn.getElementById("confirm_pin");
